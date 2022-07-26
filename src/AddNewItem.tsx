6ea13d12f2312f203994b83c89eb767f044527da
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-07-26 20:39:40 +0200

    onAdd is a callback function that will be called when we click the Create item button, toggleButtonText is the text we’ll render when this component is a button.dark is a flag that we’ll pass to the styled component.

--- src/AddNewItem.tsx
+++ src/AddNewItem.tsx
@@ -1,2 +1,8 @@
 import { useState } from "react";
 import { AddItemButton } from "./styles";
+
+type AddNewItemProps = {
+    onAdd(text: string) : void
+    toggleButtonText : string
+    dark?: boolean
+}
