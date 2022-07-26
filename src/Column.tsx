0dc6c09d47f38b86235e319c9d18f07c8b65c410
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-07-26 20:13:10 +0200

    Created the Column container and column title

---
+++ src/Column.tsx
@@ -0,0 +1,16 @@
+import { ColumnContainer, ColumnTitle } from "./styles"
+
+type ColumnProps = {
+    text: string
+}
+
+
+export const Column = ({text} : ColumnProps ) => {
+    return (
+        <ColumnContainer>
+            <ColumnTitle>
+                 {text}
+            </ColumnTitle>
+        </ColumnContainer>
+    ) 
+}
