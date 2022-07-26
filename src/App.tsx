90b01b8a4ad7e7b26028471978fb8223faba6386
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-07-26 19:45:10 +0200

    Here we specify a grey background, margins, and paddings, and also specify flex-grow: 0 so the component doesn’t try to take up all the horizontal space.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import styled from "styled-components";
+import { AppContainer } from "./styles";
 
-import './App.css';
-
-function App() {
+export const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+
+    </AppContainer>
+  )
 }
 
-export default App;
