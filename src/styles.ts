59de1e887bdae9bf1d2f75c0cabe581d299e9612
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-07-26 19:49:29 +0200

    Here we want to let the user know that cards are interactive so we specify cursor: pointer. We also want our cards to look nice so we add a box-shadow.

--- src/styles.ts
+++ src/styles.ts
@@ -26,3 +26,13 @@ export const ColumnTitle = styled.div`
     font-weight: bold;
 `
 
+export const CardContainer = styled.div`
+    background-color: #fff;
+    cursor: pointer;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem 1rem;
+    max-width: 300px;
+    box-shadow: #091e4240 0px 1px 0px 0px;
+
+`
+
